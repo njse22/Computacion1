aeca23bed8f2a82d8e3e1eb84ff039035d808ca0
njse22 <njse22@gmail.com>
2024-10-03 15:01:29 -0500

    Upload Classpath task to copy dependencies

--- PublisherSuscriberIce/server/src/main/java/Server.java
+++ PublisherSuscriberIce/server/src/main/java/Server.java
@@ -6,6 +6,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
+import com.google.gson.*;
+
 public class Server {
     public static void main(String[] args) {
 
@@ -14,6 +16,8 @@ public class Server {
 	/////////////////////////////////////////////
         try(Communicator communicator = Util.initialize(args, "properties.cfg")){
             ObjectAdapter adapter = communicator.createObjectAdapter("services");
+
+	    Gson gson =  new Gson();
 
             // Creación de los servicios de Ice
 	    // Se pueden caragar con el Object de Ice: 
