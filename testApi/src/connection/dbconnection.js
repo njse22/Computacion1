9e32011fd2706b956446c6e1ea641c72c131dd48
njse22 <njse22@gmail.com>
2024-10-24 11:29:49 -0500

    Update API res

---
+++ testApi/src/connection/dbconnection.js
@@ -0,0 +1,24 @@
+// librería para leer rutas 
+// https://nodejs.org/en/learn/manipulating-files/nodejs-file-paths
+const path = require("path")
+
+// librería para manipular archivos 
+const fs = require("fs")
+
+// __dirname = ruta actual del modulo 
+// ../.. = de la ruta actual del modulo dos carpetas atras
+// data = carpeta data
+// database.json = el archivo 
+const dbPath = path.join(__dirname, "../..", 'data', 'database.json')
+
+
+const DBConnection = {
+    readDB: () => {
+	const data = fs.readFileSync(dbPath, 'utf-8')
+	return JSON.parse(data)
+    }, 
+}
+
+module.exports = DBConnection
+
+
--- testApi/src/index.js
+++ testApi/src/index.js
@@ -1,12 +1,71 @@
+// se importa la librería necesaria para la creación del API
 const express = require("express")
+
+// Modulos de la aplicación 
+const user = require("./controller/user")
+const db = require("./connection/dbconnection")
+
 const app = express()
+
+// Se usa para leer el "body" de la solicitud del cliente
+app.use(express.json())
+
 const port = 5000
 
-app.get("/", (res, req) => {
+////////////////////////////////////////////////////////////////////////
+//                    Creación de los métodos/verbos GET              //
+////////////////////////////////////////////////////////////////////////
 
+// se define la ruta del método GET
+// necesita ruta: /db y una función (req, res) => {}
+// 
+app.get("/db", (req, res) => {
+
+    res.status(200).send("Here")
+    
+    // uso de un modulo propio para leer un archivo
+    const data = db.readDB()
+
+    console.log(data.name)
+    console.log(data.age)
+})
+
+// uso de un modulo propio para gestionar usuarios
+app.get("/users", user.list )
+
+// :id -> string = 23, LLL, nombre, ABFBBC
+app.get("/users/:id", user.get )
+
+app.post("/users/:id", user.modify )
+
+
+app.get("/", (req, res) => {
+    
+    // 200 -> OK 
+    res.status(200).send("Hola desde /")
     console.log("Hola")
     console.log(req)
 })
 
+app.post("/", (req, res) => {
+    // 201 -> Ok -> Created
+    res.status(201).send('Mensaje tipo POST')
+})
+
+app.put("/", (req, res) => {
+    // 204 -> Ok -> No Content
+    console.log(req.method)
+    res.status(204).send("Mensaje desde el PUT")
+})
+
+app.patch("/", (req, res) => {
+    console.log(req.method)
+    res.status(204).send("Pathc")
+})
+
+app.delete("/", (req, res) => {
+    console.log(req.method)
+    res.status(204).send("Deleted ")
+})
 
 app.listen(port)
